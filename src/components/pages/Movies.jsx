c20d4caeeaecf167a249b87f28667416209f6518
unknown <rachaelvallee2019@gmail.com>
2026-08-15 10:31:53 -0500

    added code

--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import MovieInfo from "../ui/MovieInfo.jsx";
 
 const Movies = () => {
-  const [movies, setMovies] = useState("fast");
+  const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("fast");
 
   useEffect(() => {
